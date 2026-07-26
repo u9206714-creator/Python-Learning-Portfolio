6f689ed2dc80e1afb8bc857d520fe1f2ca0462d2
u9206714-creator <u9206714@gmail.com>
2026-07-26 13:51:14 +0800

    input practice

---
+++ input.py
@@ -0,0 +1,14 @@
+'''
+user_weight = float(input('请输入你的体重(单位:kg):'))
+user_height = float(input('请输入你的身高(单位:m):'))
+user_BMI = user_weight/((user_height)**2)
+print('你的BMI值为'+str(user_BMI))
+'''
+'''
+mood_index = int(input('对象今天的心情指数是： '))
+if mood_index >= 60:
+    print('恭喜，今晚可以打游戏')
+    print('happy')
+else: #mood_index<60
+    print('为了自己的小命，还是别打了')
+'''
