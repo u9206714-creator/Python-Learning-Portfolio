6d0ac2bef12fb22096f2aadf8507ffc0086b0c67
u9206714-creator <u9206714@gmail.com>
2026-07-26 13:37:44 +0800

    learn data types

---
+++ data_types.py
@@ -0,0 +1,24 @@
+#对字符串求长度
+s ="Hello world!" 
+print(len(s))
+
+#通过索引获取单个字符串
+print([3])
+print([len(s)-4])
+
+#布尔类型
+b1=True
+b2=False
+
+#空值类型
+n=None
+
+#type函数
+print(type(s))
+print(type(b1))
+print(type(n))
+print(type(6.0))
+
+
+
+
